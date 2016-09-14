5b5f0e3fb2fae6cb565ad6b2e40909d08e0b253e
Alex Pereira <alex.pereira@outlook.com>
2016-09-13 20:12:01 -0400

    first commit

---
+++ app/src/main/java/com/alexpereira/www/helloworld/Question.java
@@ -0,0 +1,32 @@
+package com.alexpereira.www.helloworld;
+
+/**
+ * Created by alexpereira on 9/13/16.
+ */
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+}
