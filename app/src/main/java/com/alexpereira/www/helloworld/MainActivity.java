3c67bc5d4b9e1ec1eb55340b0a91bf59d49b22b4
Alex Pereira <alex.pereira@outlook.com>
2016-09-30 12:33:35 -0400

    Initial commit

---
+++ app/src/main/java/com/alexpereira/www/helloworld/MainActivity.java
@@ -0,0 +1,125 @@
+package com.alexpereira.www.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+// Sub-class of AppCompatActivity
+public class MainActivity extends AppCompatActivity {
+
+    //Create a button variables
+    private Button mTrueButton;
+    private Button mFalseButton;
+
+    private ImageButton mNextButton;
+    private ImageButton  mPreviousButton;
+    private TextView mQuestionTextView;
+
+    private Question[] mQuestionBank = new Question[] {
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true),
+    };
+    private int mCurrentIndex = 0;
+
+
+
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+    }
+
+
+    private void checkAnswer(boolean userPressedTrue) {
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+        int messageResId = 0;
+        if (userPressedTrue == answerIsTrue) {
+            messageResId = R.string.correct_toast;
+        } else {
+            messageResId = R.string.incorrect_toast;
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
+                .show();
+    }
+
+    @Override // Overring the onCreate from AppCompatActivity
+                            //Bundle is a class
+                            //savedInstanceState is an instance of class Bundle
+    protected void onCreate(Bundle savedInstanceState) {
+        // Sending the Bundle that we just got
+        super.onCreate(savedInstanceState); //Do this first so that settings happen first
+                       // Resource takes all of your info and assigns an ID to a task?
+        setContentView(R.layout.activity_main);
+
+
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                checkAnswer(true);
+            }
+        });
+
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                checkAnswer(false);
+            }
+        });
+
+
+        mNextButton = (ImageButton) findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+
+
+        // CHALLENGES HOMEWORK
+
+
+        //Challenge: Add a Listener to the TextView
+        mQuestionTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+
+        //Challenge: Add a Previous Button
+        mPreviousButton = (ImageButton) findViewById(R.id.previous_button);
+        mPreviousButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex - 1) % mQuestionBank.length;
+
+                if(mCurrentIndex < 0)
+                    mCurrentIndex = mQuestionBank.length - 1;
+
+                updateQuestion();
+            }
+        });
+
+        updateQuestion();
+
+        //Challenge: From Button to ImageButton
+        //See activity_main.xml
+    }
+
+
+}
